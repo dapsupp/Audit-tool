2fba817fa045600cdf3da89bb79aec79aad48a59
dapsupp <drew.smith@upp.ai>
2025-02-17 21:38:18 +0000

    Update # auth.py

--- # auth.py
+++ # auth.py
@@ -1,3 +1,4 @@
+# auth.py
 import streamlit as st
 import streamlit_authenticator as stauth
 
@@ -6,17 +7,17 @@ def get_authenticator():
     names = ["Staff One", "Staff Two"]
     usernames = ["staff1", "staff2"]
     passwords = ["password1", "password2"]
-    
-    # Generate hashed passwords
-    hashed_passwords = stauth.Hasher(passwords).generate()
-    
+
+    # Generate hashed passwords using the new method name
+    hashed_passwords = stauth.Hasher(passwords).generate_hashes()
+
     credentials = {
         "usernames": {
             "staff1": {"name": names[0], "password": hashed_passwords[0]},
             "staff2": {"name": names[1], "password": hashed_passwords[1]},
         }
     }
-    
+
     # Create and return the authenticator object
     authenticator = stauth.Authenticate(credentials, "cookie_name", "signature_key")
     return authenticator
